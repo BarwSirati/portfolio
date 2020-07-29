3b01c17641df99dd06616405b0a40a1c8ef4937c
Sirati <sirati10546@gmail.com>
2020-07-29 10:28:04 +0700

    fix

--- components/Gallery/GalleryHead.js
+++ components/Gallery/GalleryHead.js
@@ -7,7 +7,7 @@ function GalleryHead({ title }) {
       <h1 className="text-center font-size-5">{title}</h1>
       <div
         className="mx-auto text-center"
-        style={{ display: "flex", position: "relative", marginTop: "30px" }}
+        style={{ display: "flex", marginTop: "30px" }}
       >
         <div className="w-50">
           <div className="line1"></div>
